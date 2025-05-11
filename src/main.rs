c3cf6d39734119d24d078245fa6b09fee922834b
baldoqui <arthur.b.bergamo@gmail.com>
2025-05-10 21:46:47 -0300

    feat(plugin-setup): add plugins to app

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,15 @@ mod animation;
 mod setup;
 mod ui;
 
+use setup::plugin::SetupPlugin;
+use ui::plugin::UiPlugin;
+use animation::plugin::AnimationPlugin;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugins(SetupPlugin)
+        .add_plugins(UiPlugin)
+        .add_plugins(AnimationPlugin)
         .run();
 }
