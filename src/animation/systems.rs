47f7515a99189f3dbf3269e467218d62e6507868
baldoqui <arthur.b.bergamo@gmail.com>
2025-05-10 21:46:19 -0300

    feat(animation): add animation plugin

---
+++ src/animation/systems.rs
@@ -0,0 +1,23 @@
+use super::components::*;
+use bevy::prelude::*;
+
+pub fn trigger_animation<S: Component>(mut animation: Single<&mut AnimationConfig, With<S>>) {
+    animation.frame_timer = AnimationConfig::timer_from_fps(animation.fps);
+}
+
+pub fn execute_animations(time: Res<Time>, mut query: Query<(&mut AnimationConfig, &mut Sprite)>) {
+    for (mut config, mut sprite) in &mut query {
+        config.frame_timer.tick(time.delta());
+
+        if config.frame_timer.just_finished() {
+            if let Some(atlas) = &mut sprite.texture_atlas {
+                if atlas.index == config.last_sprite_index {
+                    atlas.index = config.first_sprite_index;
+                } else {
+                    atlas.index += 1;
+                    config.frame_timer = AnimationConfig::timer_from_fps(config.fps);
+                }
+            }
+        }
+    }
+}
